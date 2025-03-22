8d55a59c5166e2f9fa7d0390bc93f01adb0b5bf9
Lilia Brown <160168216+Lilia-Brown@users.noreply.github.com>
2025-03-22 16:02:18 -0700

    GETS all restaurants from BE API and renders on Restaurants.jsx

--- app/javascript/components/Restaurants/Restaurants.jsx
+++ app/javascript/components/Restaurants/Restaurants.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Restaurants = () => {
+  const [restaurants, setRestaurants] = useState([])
+  useEffect(() => {
+    axios.get('/api/v1/restaurants.json')
+      .then(response => {
+        setRestaurants(response.data.data)
+      })
+      .catch(error => {
+        console.error('There was an error fetching the restaurants!', error);
+      });
+  }, [])
   return (
     <div>
       <h1>Restaurants</h1>
-      <p>Here are all the restaurants!</p>
+      <ul>
+        {restaurants.map(restaurant => (
+          <li key={restaurant.attributes.id}>{restaurant.attributes.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
