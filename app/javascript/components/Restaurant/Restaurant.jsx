5f9a77cfc6ecb6912547804bf5fa272e6181fbce
Lilia Brown <160168216+Lilia-Brown@users.noreply.github.com>
2025-03-22 16:37:14 -0700

    STYLES Restaurant page

--- app/javascript/components/Restaurant/Restaurant.jsx
+++ app/javascript/components/Restaurant/Restaurant.jsx
@@ -1,8 +1,43 @@
-import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import React, { Fragment, useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  max-width: 600px;
+  padding: 20px;
+`
+const Header = styled.div`
+  color: #333;
+  font-size: 2rem;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`
+const Image = styled.div`
+  img {
+    height: 100px;
+    width: 100px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 10%;
+    padding: 10px;
+  }
+`
+const Table = styled.ul`
+  text-align: left;
+  list-style-type: none;
+  padding-left: 100px;
+  li {
+    margin: 10px 0;
+    font-size: 1rem;
+  }
+`
 
 const Restaurant = () => {
+  const [loaded, setLoaded] = useState(false)
   const [restaurant, setRestaurant] = useState([])
+
   useEffect(() => {
     const slug = window.location.pathname.split('/')[2]
     axios.get(`/api/v1/restaurants/${slug}.json`)
@@ -11,17 +46,35 @@ const Restaurant = () => {
       })
       .catch(error => {
         console.error('There was an error fetching the restaurant!', error);
-      });
+      })
+      .finally(setLoaded(true))
   }, [])
+
   return (
-    <div>
-      <h1>{restaurant.name}</h1>
-      <ul>
-        <li key='cuisine'>{restaurant.cuisine}</li>
-        <li key='website'>{restaurant.website}</li>
-      </ul>
-      <img src={restaurant.img_url} alt={restaurant.name} width='400' />
-    </div>
+    <Wrapper>
+      {
+        loaded &&
+        <Fragment>
+          <Header>{restaurant.name}</Header>
+          <Image>
+            <img src={restaurant.img_url} alt={restaurant.name} />
+          </Image>
+          <Table>
+            <li key='cuisine'>Cuisine:&nbsp;
+              {restaurant.cuisine}
+            </li>
+            <li key='website'>Website:&nbsp;
+              <Link to={restaurant.website} target='_blank' rel='noopener noreferrer'>
+                {restaurant.website}
+              </Link>
+            </li>
+            <li key='address'>Address:&nbsp;
+              {restaurant.address}
+            </li>
+          </Table>
+        </Fragment>
+      }
+    </Wrapper>
   )
 }
 
