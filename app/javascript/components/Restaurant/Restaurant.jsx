a66ba0e1e0d1954af246806bdd688286b44a5492
Lilia Brown <160168216+Lilia-Brown@users.noreply.github.com>
2025-03-22 16:03:44 -0700

    GETS specific restaurant by slug from BE API and renders on Restaurant.jsx

--- app/javascript/components/Restaurant/Restaurant.jsx
+++ app/javascript/components/Restaurant/Restaurant.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Restaurant = () => {
+  const [restaurant, setRestaurant] = useState([])
+  useEffect(() => {
+    const slug = window.location.pathname.split('/')[2]
+    axios.get(`/api/v1/restaurants/${slug}.json`)
+      .then(response => {
+        setRestaurant(response.data.data.attributes)
+      })
+      .catch(error => {
+        console.error('There was an error fetching the restaurant!', error);
+      });
+  }, [])
   return (
     <div>
-      <h1>Restaurant</h1>
-      <p>Welcome to the restaurant page!</p>
+      <h1>{restaurant.name}</h1>
+      <ul>
+        <li key='cuisine'>{restaurant.cuisine}</li>
+        <li key='website'>{restaurant.website}</li>
+      </ul>
+      <img src={restaurant.img_url} alt={restaurant.name} width='400' />
     </div>
   )
 }
